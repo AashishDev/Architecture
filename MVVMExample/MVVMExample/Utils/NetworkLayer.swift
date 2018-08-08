c185f45633eabe02ae280e05699200f8d643ccd7
Aashish Tyagi <aashish.tyagi@crmnext.in>
2018-08-08 14:40:50 +0530

    Integrate Almofire and SwiftyJson

---
+++ MVVMExample/MVVMExample/Utils/NetworkLayer.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkLayer.swift
+//  MVVMExample
+//
+//  Created by MOBILE MAC1 on 8/8/18.
+//  Copyright © 2018 MOBILE MAC1. All rights reserved.
+//
+
+import UIKit
+
+class NetworkLayer: NSObject {
+
+}
